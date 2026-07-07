47ae833c573d75f3c0eb2d0986be36da487d3089
saimedh <saimedhp@gmail.com>
2026-07-07 22:15:28 +0530

    Fix Render deploy: update start script and exclude dotenv in prod

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+}
 const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
